a2b883f2e368d0da217f8eda6e8226722b893fab
KouE10e <210100037+dmk2E@users.noreply.github.com>
2025-09-06 20:09:51 +0900

    共通コンポーネントお試し用

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
 import CssBaseline from "@mui/material/CssBaseline"; //ブラウザのデフォルトのスタイルを打ち消す用
 import OneLineInputField from "./components/OneLineInputField/OneLineInputField.tsx";
+import TextInputField from "./components/TextInputField/TextInputField.tsx";
+import Button from "./components/Button/Button.tsx";
+import "./util.css";
 
 function App() {
   return (
@@ -7,6 +10,8 @@ function App() {
       <CssBaseline />
       <div>
         <OneLineInputField placeholder="Input your name." setText={(text:string)=>{}}/>
+        <TextInputField placeholder="Input your name." rows={5} cols={10} setText={(text:string)=>{}}/>
+        <Button text="Submit" onClickFunc={()=>{}}/>
       </div>
     </>
   )
