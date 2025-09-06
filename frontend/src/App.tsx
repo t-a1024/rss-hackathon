8c9e708c08d853a18b4b63f44b315869756d0ed4
KouE10e <210100037+dmk2E@users.noreply.github.com>
2025-09-06 18:49:45 +0900

    Material UI のインストール

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from 'react';
+import OneLineInputField from "./components/OneLineInputField/OneLineInputField.tsx";
 
 function App() {
   return (
     <>
+      <div>
+        <OneLineInputField placeholder="Input your name." setText={(text:string)=>{}}/>
+      </div>
     </>
   )
 }
