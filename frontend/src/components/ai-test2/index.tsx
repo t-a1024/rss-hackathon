9e71bce347b982284e975eda9d2d14b122288e3e
AKITO KOHNO <akito.0424.jp@gmail.com>
2025-09-06 23:50:13 +0900

    ai-api

---
+++ frontend/src/components/ai-test2/index.tsx
@@ -0,0 +1,5 @@
+// 部屋API テスト用コンポーネントのエントリーポイント
+export { default as RoomTestApp } from './RoomTestApp';
+export { default as RoomCreator } from './RoomCreator';
+export { default as RoomAnswerForm } from './RoomAnswerForm';
+export { default as RoomResultDisplay } from './RoomResultDisplay';
