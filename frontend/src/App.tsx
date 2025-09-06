8aad9547bcf91b1272454d2f787ba8a476c652aa
Satoshi Tomita <yamatomi3104@gmail.com>
2025-09-06 23:01:10 +0900

    api呼び出し完了

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
 import { Link, Outlet } from "react-router-dom";
 import "./util.css";
-
+import Home from "./pages/Home";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export default function App() {
   return (
     <main className="p-6">
       <Outlet />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="light"  // 好みで "colored" など
+      />
+      <Home/>
     </main>
   );
 }
