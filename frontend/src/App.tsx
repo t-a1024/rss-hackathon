5b8233e84b476f20f133facf710b5fbdcad25ead
KouE10e <210100037+dmk2E@users.noreply.github.com>
2025-09-06 17:17:56 +0900

    TextInputFieldとButtonお試し用

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './components/Button/Button.tsx';
+import TextInputField from './components/TextInputField/TextInputField.tsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,12 @@ function App() {
   return (
     <>
       <div>
+        <div>
+          <Button text="Submit" onClickFunc={()=>{}}/>
+        </div>
+        <div>
+          <TextInputField setText={(text:string)=>{}} rows={3} cols={9} placeholder="Input your name"/>
+        </div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
