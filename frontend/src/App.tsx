a0a7cee302f14d4c84511e405fa4c5ecf8be279c
KouE10e <210100037+dmk2E@users.noreply.github.com>
2025-09-06 19:02:12 +0900

    ブラウザのデフォルトスタイルの打ち消し用

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
+import CssBaseline from "@mui/material/CssBaseline"; //ブラウザのデフォルトのスタイルを打ち消す用
 import OneLineInputField from "./components/OneLineInputField/OneLineInputField.tsx";
 
 function App() {
   return (
     <>
+      <CssBaseline />
       <div>
         <OneLineInputField placeholder="Input your name." setText={(text:string)=>{}}/>
       </div>
